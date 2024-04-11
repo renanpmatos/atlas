5518445c1590cb311bce4aae97c3d6ad7d431e65
renanpmatos <renan.p.matos1@gmail.com>
2024-04-11 20:38:58 -0300

    adding close case in badge

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,10 @@
-import { Badge } from "@/components/ui/badge";
-import { Check } from "lucide-react";
+import Badges from "./(exemples)/badges";
 
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="flex flex-col gap-5">
-        <div className="flex flex-row gap-20">
-          <Badge content={"teste"} className="right-10" />
-          <Badge
-            content={"5"}
-            variant={"circle"}
-            placement="tright"
-            className="bg-rose-500">
-            <img
-              src={"https://i.pravatar.cc/300?u=a042581f4e29026709d"}
-              width={100}
-              height={100}
-              alt="Picture of the author"
-              className="rounded-full border-double border-8 border-gray-600"
-            />
-          </Badge>
-          <Badge content={"5"} placement="tleft">
-            <img
-              src={"https://i.pravatar.cc/300?u=a042581f4e29026709d"}
-              width={100}
-              height={100}
-              alt="Picture of the author"
-              className="rounded-2xl"
-            />
-          </Badge>
-          <Badge
-            placement="bright"
-            icon={<Check />}
-            className="rounded-full px-1 py-1.5 bg-green-500">
-            <img
-              src={"https://i.pravatar.cc/300?u=a042581f4e29026709d"}
-              width={100}
-              height={100}
-              alt="Picture of the author"
-              className="rounded-2xl border-double border-8 border-green-500"
-            />
-          </Badge>
-          <Badge content={"5"} placement="bleft">
-            <img
-              src={"https://i.pravatar.cc/300?u=a042581f4e29026709d"}
-              width={100}
-              height={100}
-              alt="Picture of the author"
-              className="rounded-2xl"
-            />
-          </Badge>
-        </div>
-        <div className="flex flex-row gap-5"></div>
-        <div className="flex flex-row gap-5"></div>
+        <Badges />
       </div>
     </div>
   );
--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,16 +1,24 @@
+"use client";
+
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
-import { IconType } from "react-icons";
+import { X } from "lucide-react";
 
 const badgeVariants = cva(
   "absolute flex items-center justify-center rounded-full border text-base font-semibold transition-colors",
   {
     variants: {
+      disabled: {
+        true: "pointer-events-none opacity-75",
+      },
       variant: {
         default:
           "px-2.5 py-1 border-transparent bg-primary text-primary-foreground",
-        outline: "text-foreground",
+        outline:
+          "px-2.5 py-1 border-2 border-primary rounded-2xl bg-background text-accent-foreground",
+        shadow:
+          "px-2.5 py-1 border-transparent bg-primary text-primary-foreground shadow-lg shadow-primary/30",
         circle:
           "rounded-full px-2.5 py-1 m-2.5 bg-primary text-primary-foreground",
       },
@@ -35,17 +43,23 @@ const badgeVariants = cva(
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {
+  disabled?: boolean;
   content?: string;
-  icon?: React.ReactNode; // Aceita um tipo de ícone do React Icons
+  icon?: React.ReactNode;
+  onClose?: () => void;
+  closeVariant?: "default" | "round";
 }
 
 function Badge({
   children,
   className,
+  disabled,
   content,
   icon,
   placement,
   variant,
+  onClose,
+  closeVariant,
   ...props
 }: BadgeProps) {
   return (
@@ -53,11 +67,29 @@ function Badge({
       {/* div pai, usado como referencia para posicionar a badge */}
       <div className="relative">
         <div
-          className={cn(badgeVariants({ placement, variant }), className)}
+          className={cn(
+            badgeVariants({ disabled, placement, variant }),
+            className
+          )}
           {...props}>
           {/* Renderiza o ícone se estiver disponível */}
           {icon && <span className="mr-1">{icon}</span>}
           {content}
+
+          {/* Renderiza o botão de fechar se a função onClose estiver presente */}
+          {onClose && (
+            <button
+              className={cn(
+                "flex ml-2",
+                closeVariant == "round"
+                  ? "bg-background/50 rounded-full hover:bg-background hover:text-primary transition ease-in-out duration-200"
+                  : ""
+              )}
+              onClick={onClose}
+              aria-label="close">
+              <X className={cn("p-0.5")} />
+            </button>
+          )}
         </div>
         {children}
       </div>
